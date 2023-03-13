48be00ee03c179532d7737359242c2cf805dfad8
Summit Gupta <summitgupta07@gmail.com>
2023-03-13 13:29:44 +0530

    EJ

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// design file
+// app.use(express.static("public"));
+app.use("/public/", express.static("./public"));
+app.set("view engine", "ejs");
+// app.use(express.static("asset"));
+
+// routers
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// server listening
+app.listen(PORT, () => {
+  console.log(`The app start on http://localhost:${PORT}`);
+});
